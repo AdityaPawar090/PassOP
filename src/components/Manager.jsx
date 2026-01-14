0195bf86b895ba39a9a1060e19c71df57fa0438a
Aditya <adityadpawar090@gmail.com>
2026-01-14 23:18:13 +0530

    completed with basic ui

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,21 +1,48 @@
 import React from 'react'
+import { useRef } from 'react';
 
 const Manager = () => {
+    const ref = useRef()
+    const showPassword = (params) => {
+        alert("show the password");
+        if(ref.current.src.includes("/hidden.png")){
+            ref.current.src = "/eye.png" 
+        }
+        else{
+            ref.current.src = "/hidden.png" 
+        }
+    }
   return (
     <>
-    <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+    <div className="fixed inset-0 -z-10 bg-green-50  bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
 
-    <div className='container mx-auto bg-slate-50 mycontainer'>
 
-        <h1>PassOP</h1>
-        <p>Your own Password Manager</p>
+    <div className='mycontainer'>
 
-        <div className='text-white flex flex-col p-4'>
-            <input className='rounded-full' type="text" name="" id="" />
-            <div className='flex'>
-                <input type="text" />
-                <input type="text" />
+        <h1 className='text-4xl font-bold text-center'><span className='text-green-700'>/&lt;</span>
+        Pass
+        <span className='text-green-500'>OP/&gt;</span></h1>
+        <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
+
+        <div className='text-white flex flex-col p-4 text-black gap-8 items-center'>
+            <input placeholder='Enter Website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="" id=""/>
+            <div className='flex w-full justify-between gap-8'>
+                <input placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="" id=""/>
+
+                <div className='relative w-full'>
+
+                <input placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="" id=""/>
+                <span className='absolute right-[3px] top-[4px] cursor-pointer' onClick={showPassword}>
+                   <img ref={ref} className='p-1' width={26} src="/eye.png" alt="eye" />
+                </span>
+                </div>
             </div>
+            <button className='flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900'> <lord-icon
+                        src="https://cdn.lordicon.com/bjxtqill.json"
+                        trigger="hover"
+                        colors="primary:#121331,secondary:#242424">
+                    </lord-icon>
+            Add Password</button>
         </div>
     </div></>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="bg-slate-800 text-white">
-      <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+      <div className=" flex justify-between items-center px-4 py-5 h-14">
 
         <div className="logo font-bold text-white text-2xl flex items-center">
           <span className='text-green-700'>/&lt;</span>
@@ -12,9 +12,9 @@ const Navbar = () => {
         </div>
      <ul>
       <li className='flex gap-4'>
-        <a className='hover: font-bold' href="/">Home</a>
-        <a className='hover: font-bold' href="#">About</a>
-        <a className='hover: font-bold' href="#">Contact</a>
+        <a className='hover:font-bold' href="/">Home</a>
+        <a className='hover:font-bold' href="#">About</a>
+        <a className='hover:font-bold' href="#">Contact</a>
       </li>  
       </ul> 
       </div>
